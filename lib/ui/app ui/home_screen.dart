2d9fc6a99a8a6f552b98688f7bbef2cde986af26
MSI GL65 <najy86275@gmail.com>
2022-07-23 13:50:04 -0800

    another commit

--- lib/ui/app ui/home_screen.dart
+++ lib/ui/app ui/home_screen.dart
@@ -5,6 +5,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+       child: Text('hhell')
+      ),
+    );
   }
 }
